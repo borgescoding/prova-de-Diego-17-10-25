efcab73d1d0ba04d2548df49c5d9f32c9ddd1a87
borgescoding <borgesmicael876@gmail.com>
2025-10-17 17:07:40 -0300

    Add files via upload

---
+++ Atvdd09/Atvdd09/Program.cs
@@ -0,0 +1,19 @@
+﻿{ 
+    string[] nomes = { "Micael", "Diego", "Pedro", "Isabelly", "Lucas" };
+
+    Console.Write("Digite um nome para pesquisar: ");
+    string nomeBusca = Console.ReadLine();
+
+    foreach (string nome in nomes)
+    {
+        if (nome.Equals(nomeBusca)) 
+        {
+            Console.WriteLine($"O nome {nomeBusca} foi encontrado no vetor.");
+            return; 
+        }
+        else
+        {
+            Console.WriteLine("O nome não foi encontrado");
+        }
+    }
+}
