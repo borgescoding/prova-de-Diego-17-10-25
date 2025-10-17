1ae6ee90e968cbb5c7d500709a67532bb37f8879
borgescoding <borgesmicael876@gmail.com>
2025-10-17 17:10:04 -0300

    Add files via upload

---
+++ atvdd7/atvdd7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+Console.Write("Digite o número inicial: ");
+int start = int.Parse(Console.ReadLine());
+
+Console.Write("Digite o número final: ");
+int end = int.Parse(Console.ReadLine());
+
+for (int i = start; i <= end; i++)
+{
+    Console.WriteLine($"\nTabuada do {i}:");
+for (int j = 1; j <= 10; j++)
+{
+Console.WriteLine($"{i} x {j} = {i * j}");
+}
+}
